ee0d00da2429530f982f54a3f10d213afa8439d3
Nurdiansah <nurdiansah14045@gmail.com>
2022-05-16 14:39:42 +0700

    inputan

--- views/superuser/page.php
+++ views/superuser/page.php
@@ -6,4 +6,6 @@ if ($page == 'formpesan') {
     include_once "formpesan.php";
 } else if ($page == "") {
     include_once "main.php";
+} else if ($page == 'form_preuse') {
+    include_once "form_preuse.php";
 }
